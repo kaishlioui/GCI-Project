0ba1533638888819aa25158e673c4ce85285c1ca
Kais Hlioui <kais.hlioui@gmail.com>
2023-01-09 01:00:29 +0300

    add the readme file

--- src/components/OptionItem/OptionItem.js
+++ src/components/OptionItem/OptionItem.js
@@ -8,7 +8,7 @@ import { StyledOptionItem } from "./OptionItem.styled";
 const OptionItem = ({ id, price, name, isAddedToCart }) => {
   const { addCartItem } = useContext(OptionContext);
   return (
-    <StyledOptionItem>
+    <StyledOptionItem role="article">
       <img className="option-item__img" src={`/${id}.jpg`} alt={name} />
       <h3 className="option-item__name">{name}</h3>
       <div className="option-item__price">€{price}</div>
